1e4c2f82b10869eba7dfe4bb6d7b7257bd05a8ae
Tadhg Lynch <58711539+Lyncht98@users.noreply.github.com>
2023-04-17 20:53:43 -0400

    simple, webapp done

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from shiny import ui, render, App
-import socket
 
 app_ui = ui.page_fluid(
     ui.input_slider("n", "N", 0, 100, 40),
@@ -15,15 +14,7 @@ def server(input, output, session):
 # This is a shiny.App object. It must be named `app`.
 app = App(app_ui, server)
 
-def print_local_address():
-    # Get the local address of the computer
-    local_address = socket.gethostbyname(socket.gethostname())
-
-    # Print the local address
-    print("My local address is:", local_address)
-
 if __name__ == "__main__":
 
 # Call the print_local_address() function
-    print_local_address()
     app.run()
